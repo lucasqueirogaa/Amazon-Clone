4fc0772d72282dd26d88a96cc2ee078695bbc22a
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-08-07 08:57:30 -0300

    feat: Finish checkout button and calculate all price

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,11 +3,15 @@ import Header from "../components/Header";
 import Head from "next/dist/next-server/lib/head";
 import Image from "next/image";
 import { useSelector } from "react-redux";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/react";
 
 function Checkout() {
   const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const { data } = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -39,7 +43,29 @@ function Checkout() {
         </div>
 
         {/* Right */}
-        <div></div>
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowrap">
+                Subtotal ({items.length} items):
+                <span className="font-bold">
+                  {" "}
+                  <Currency quantity={total} currency="GBP" />
+                </span>
+              </h2>
+
+              <button
+                disabled={!data}
+                className={`button mt-2 ${
+                  !data &&
+                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                }`}
+              >
+                {!data ? "Sign in to checkout" : "Proceed to checkout"}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
