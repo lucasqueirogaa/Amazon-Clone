a34f7e8ea5381391e8678910447d73a26ef0fa62
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-08-10 12:17:49 -0300

    feat: Create the checkout redirect to the stripe

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -7,11 +7,29 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const { data } = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: data.user.email,
+    });
+
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) alert(result.error.message);
+  };
 
   return (
     <div className="bg-gray-100">
@@ -55,6 +73,7 @@ function Checkout() {
               </h2>
 
               <button
+                onClick={createCheckoutSession}
                 disabled={!data}
                 className={`button mt-2 ${
                   !data &&
